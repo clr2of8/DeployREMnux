d806e3f4a2734f4f6c11bf35fa07c92b13f499d2
clr2of8 <clr2of8@gmail.com>
2018-01-27 21:25:14 -0700

    first commit

---
+++ DeployREMnux.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+from libcloud.compute.deployment import ScriptDeployment
+from libcloud.compute.base import NodeAuthSSHKey
+import datetime, json, argparse, time, string, random
+from distutils.util import strtobool
+
+
+parser = argparse.ArgumentParser(description='some')
+parser.add_argument('-p','--password',help='The password for the remnux user that will be used to RDP into the instace', required=False)
+parser.add_argument('-t','--terminate',help='Terminate the specified node id and associated resources. e.g. -t i-0cf26edddf5c39b6a', required=False)
+args = parser.parse_args()
+
+
+# read config file
+with open('DeployREMnux-config.txt') as json_data_file:
+    config = json.load(json_data_file)
+ACCESS_ID = config["AmazonConfig"]["aws_access_key_id"]
+SECRET_KEY = config["AmazonConfig"]["aws_secret_access_key"]
+private_key_file = config["SshConfig"]["private_key_file"]
+public_key_file = config["SshConfig"]["public_key_file"]
+
+myregion = 'us-east-1'
+IMAGE_ID = 'ami-ede8d797'
+SIZE_ID = config["AmazonConfig"]["aws_instance_size"]
+
+cls = get_driver(Provider.EC2)
+driver = cls(ACCESS_ID, SECRET_KEY, region=myregion)
+
+def cleanup_and_exit(node):
+    print ("Terminating node and associated resources.")
+    nodeName = node.name
+    driver.destroy_node(node)
+    start_time = time.time()
+    retry_timeout = 60*2 # retry time in seconds
+    while(True):
+        try:
+            driver.ex_delete_security_group(nodeName)
+        except Exception:
+            if(time.time() - start_time > retry_timeout):
+                print "Failed to delete associated Security Group"
+                break
+            time.sleep(3)
+            continue
+        break
+    kp = driver.get_key_pair(nodeName)
+    driver.delete_key_pair(kp)
+    print("REMnux Instance Shutdown")
+    exit()
+    
+if args.terminate:
+    node = driver.list_nodes([args.terminate])[0]   
+    cleanup_and_exit(node)
+
+print("Spinning up REMnux Instance, this will take a minute or two. . .")
+
+nodeName = datetime.datetime.utcnow().strftime("REMnux-%Y-%m-%dt%H-%M-%S-%f")
+
+sizes = driver.list_sizes()
+images = driver.list_images(location=myregion, ex_image_ids=[IMAGE_ID])
+
+size = [s for s in sizes if s.id == SIZE_ID][0]
+image = [i for i in images if i.id == IMAGE_ID][0]
+ 
+#read in public key in order to deploy to new node
+with file(public_key_file) as f:
+    pub_key = f.read()
+pk = NodeAuthSSHKey(pub_key)
+ 
+# create this ssh key in the cloud
+driver.import_key_pair_from_string(nodeName, pub_key)
+   
+# create the security group, only allowing ssh at first
+sg = driver.ex_create_security_group(nodeName, "allows ssh and rdp from anywhere", vpc_id=None)
+sg = driver.ex_authorize_security_group_ingress(sg["group_id"], 22, 22, cidr_ips=['0.0.0.0/0'], group_pairs=None, protocol='tcp')
+
+# a deployment to change the remnux user password that will be used for RDP access
+# Generate a random password if a blank password was provided in the configuration file, Random string generator from https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
+newPass = config["RemnuxConfig"]["remnux_user_password"]
+if args.password:
+    newPass = args.password
+if newPass == "":
+    newPass = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(20))
+passwd_change_cmd = ('#!/usr/bin/env bash\n\necho -e "malware\n%s\n%s\n\" | passwd' % (newPass,newPass)).encode('utf-8')
+sd = ScriptDeployment(passwd_change_cmd, args=None, name="change_passwd.sh")
+
+my_mapping = [{'VirtualName': None, 'Ebs': {'DeleteOnTermination': 'true'}, 'DeviceName': '/dev/sda1'}]
+
+# create and provision node
+node = driver.deploy_node(name=nodeName, ex_blockdevicemappings=my_mapping, image=image, size=size, ex_security_groups=[nodeName] ,auth=pk, ssh_username="remnux",ssh_key=private_key_file,deploy=sd)
+
+# allow remote access (RDP) by modifying the security group
+driver.ex_authorize_security_group(nodeName, 3389, 3389, cidr_ip='0.0.0.0/0', protocol='tcp')
+
+print("REMnux Instance Ready to use, IP address: %s  RDP password for the 'remnux' user is %s" % (node.public_ips[0], newPass))
+print("For SSH access use this command: ssh -i %s remnux@%s" % (private_key_file, node.public_ips[0]))
+
+# prompt before instance shutdown
+print 'Would you like to shutdown the instance now? [Y/n]',
+while True:
+    try:
+        response = raw_input().lower()
+        if ((response is "") or (strtobool(response))):
+            cleanup_and_exit(node)
+            break
+        else:
+            print ("You can terminate this instance in the future by running this command:\n python DeployREMnux.py -t %s" % node.id)
+            break
+    except ValueError:
+        print "Please respond with y or n",       
+
+
+
+
---
+++ README.md
@@ -0,0 +1,50 @@
+# DeployREMnux
+
+DeployREMnux is a Python script that will deploy a cloud instance of the public REMnux distribution in the Amazon cloud (AWS).
+
+
+## Installation
+
+Make sure you have Python 2.7 installed and use this version.
+To use the script, you will need to Install the python cloud library https://libcloud.apache.org/ and two other libraries.
+
+```
+pip install apache-libcloud
+pip install paramiko
+pip install pycrypto
+```
+
+Special Considerations for usage on Windows:
+The pip tool may not be in your PATH, so you may be able to call it directly like "c:\Python27\Scripts\pip.exe install paramiko"
+You must install the "Microsoft Visual C++ 9.0" before installing pycrypto, which you can get [here](https://www.microsoft.com/en-us/download/details.aspx?id=44266)
+Unfortunately, there is a bug in deployment when using libcloud on Windows and the deployment script to change the default password does not get run. If you ran DeployREMnux from windows, you need to connect to the instance vi SSH or RDP with the default password of "malware" and manually run the change_passwd.sh script. For security reasons, this should be done immediately after deployment.
+
+Also, you'll need active Amazon EC2 access keys.  If you don't have one already, you can create one using the following steps:
+
+```
+Log into your EC2 Console: https://console.aws.amazon.com
+Select your name -> Security Credentials.
+Expand "Access Keys"
+Create New Access Key.
+Record the Access Key ID and the Access Key
+```
+
+You also need to setup your configuration files, see **DeployREMnux-config.txt.example** for an example. Make a copy of this file and rename it by removing **.example** from the end of the file name. Then update each of the configuration values as needed. At a minimum, you must specify your Amazon access keys that you generated above, as well as the location of your public and private SSH keys. If specifying a windows path to a key file use double backslashes like c:\\path\\to\\key\\id_pub
+
+Generate your private key without a passphrase.
+Note, if on Windows, generating the SSH keys via PuTTYgen is problematic. Use ssh-keygen (for example, ssh-keygen -t rsa -b 4096) to generate your keys. You can do this from Linux or through the Git Bash command line on windows.
+
+
+## Usage
+
+Use this command to spin up your REMnux instance in the cloud:
+
+```
+python DeployREMnux.py
+```
+The output of this command will tell you how to connect to your instance via SSH and RDP as well as give you an option for terminating the instance.
+
+## Watch for Orphaned Resources that may cost you money unnecessarily
+
+Always keep an eye on Amazon console (https://console.aws.amazon.com/ec2) as you run these scripts to watch for orphaned instances, volums, etc, killing them manually if necessary to avoid additional charges. You must change to the region where the resources were created (N. Virginia by default), in order to see them.
+ 
